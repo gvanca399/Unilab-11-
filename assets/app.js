cbda29e7163745668fd9bae9ddbb04692fb05705
Gvantsa Baidoshvili <64689915+gvanca399@users.noreply.github.com>
2022-09-08 20:45:32 +0400

    done my homework

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+let myName = {
+    name: "gvantsa", 
+    lastName: "baidoshvili", 
+    age: 20, 
+    profession: "georgian Philologist", 
+    tired: true, 
+}
+
+console.log(myName.name, myName.lastName, myName.age, myName.profession, myName.sad, );
